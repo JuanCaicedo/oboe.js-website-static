7078ce5e8490a26f70eb6dd4b6ad87d8261439bf
Juan Caicedo <juan@spanishdict.com>
2016-04-02 16:45:41 -0400

    build into build/

--- build-static.js
+++ build-static.js
@@ -1,10 +1,20 @@
 const Metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
+const permalinks = require('metalsmith-permalinks');
+const collections = require('metalsmith-collections');
 
 Metalsmith(__dirname)
-  .source('./content')
+  .source('./src')
+  .use(collections({
+    pages: {
+      pattern: 'pages/*.md'
+    }
+  }))
   .use(markdown())
-  .destination('./pages')
+  .destination('./build')
+  .use(permalinks({
+    pattern: ':page'
+  }))
   .build(function(err) {
     if (err) console.log(err);
   });
